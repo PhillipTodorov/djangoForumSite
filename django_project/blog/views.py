c1008b1a5035882cf4e821584d939190af9b3876
PhillipTodorov <ph.todorov@yahoo.co.uk>
2019-08-01 19:21:57 +0300

    Added update and delete on posts

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 def home(request):
@@ -16,7 +17,40 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
 	model = Post
-	
+
+class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+	model = Post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+	model = Post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = Post	
+	success_url = '/'
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
+
 
 
 def about(request):
